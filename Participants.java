1d5f6b27324676a1e6f3cbb29cd09d4b9f460de9
HANXIAN GUAN <1103692@adm.ehu.es>
2026-01-21 18:06:50 +0100

    Add [Your Name] to the participants list

--- Participants.java
+++ Participants.java
@@ -8,6 +8,7 @@ public class Participants {
         
         greet("Adrian", "Yungel1");
         greet("Mikel", "Midoiaga");
+	greet("Hanxian", "hguan001");
     }
 
     public static void greet(String name, String githubUser) {
